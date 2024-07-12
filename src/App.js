1c816e2f220235736bc3b0959b525ca9adf4f6ca
BNK <byhtada@gmail.com>
2024-07-12 22:06:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [camera, cameraState] = useState(false)
+
+    const handleClick = () => {
+        cameraState(true)
+      console.log("handleClick")
+      alert("handleClick")
+    }
+    const videoConstraints = {
+        width: 1280,
+        height: 720,
+        facingMode: "user"
+    };
+
+    return (
+      <div className="App">
+
+          <Webcam
+              audio={false}
+              height={720}
+              screenshotFormat="image/jpeg"
+              width={1280}
+              videoConstraints={videoConstraints}
+          >
+              {({ getScreenshot }) => (
+                  <button
+                      onClick={() => {
+                          const imageSrc = getScreenshot()
+                      }}
+                  >
+                      Capture photo
+                  </button>
+              )}
+          </Webcam>
+
+
+          <button onClick={handleClick}>Start</button>
+      </div>
+    );
 }
 
 export default App;
